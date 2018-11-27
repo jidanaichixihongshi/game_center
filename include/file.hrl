aae966f913f340e7c5515dac5f34c79715e71d47
root <root@localhost.localdomain>
2018-11-27 17:29:49 +0800

    proto

---
+++ include/file.hrl
@@ -0,0 +1,3 @@
+
+-define(PROTO_CONFIG, "config/protobuf.proto").
+
---
+++ include/protobuf_pb.hrl
@@ -0,0 +1,63 @@
+-ifndef(PROTO_PB_H).
+-define(PROTO_PB_H, true).
+-record(proto, {
+    mt = erlang:error({required, mt}),
+    sig = erlang:error({required, sig}),
+    router = erlang:error({required, router}),
+    data,
+    ts = erlang:error({required, ts})
+}).
+-endif.
+
+-ifndef(DATA_PB_H).
+-define(DATA_PB_H, true).
+-record(data, {
+    dt,
+    mid = erlang:error({required, mid}),
+    children = erlang:error({required, children}),
+    extend
+}).
+-endif.
+
+-ifndef(ROUTER_PB_H).
+-define(ROUTER_PB_H, true).
+-record(router, {
+    from = erlang:error({required, from}),
+    fdevice,
+    fserver,
+    to,
+    tdevice,
+    tserver
+}).
+-endif.
+
+-ifndef(CHAT_PB_H).
+-define(CHAT_PB_H, true).
+-record(chat, {
+    from,
+    device,
+    ct = erlang:error({required, ct}),
+    c = erlang:error({required, c})
+}).
+-endif.
+
+-ifndef(REQUEST_PB_H).
+-define(REQUEST_PB_H, true).
+-record(request, {
+    from,
+    device,
+    rt = erlang:error({required, rt}),
+    rm = erlang:error({required, rm}),
+    extend
+}).
+-endif.
+
+-ifndef(PUSH_PB_H).
+-define(PUSH_PB_H, true).
+-record(push, {
+    pt = erlang:error({required, pt}),
+    pm = erlang:error({required, pm}),
+    extend
+}).
+-endif.
+
